c8688b7fa77bce2e9a4019443c01b00c291282e4
Siarra Price <siarraprice@ufl.edu>
2019-10-27 21:28:16 -0500

    Show hidden element after 3 seconds

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-// It is always helpful to use comments in your code!
+//declare water constants
 const boils = "212";
 const freezes = "32";
 
+//print water temps immediately into span element
 document.getElementById("boiling-temp").innerHTML = boils;
 document.getElementById("freezing-temp").innerHTML = freezes;
+
+//pull current water temp
+let current = "51";
+
+//print current water temp into the span
+document.getElementById("water-temp").innerHTML = current;
+
+
+window.onload=function() 
+{
+	setTimeout(goodtemp, 3000);
+
+};
+function goodtemp()
+{
+	document.getElementById("good-temp").className="show";
+}
